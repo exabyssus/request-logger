34c39a91275cda7ed8bfad61fb95d1a6c1f49b5a
Martins Ungurs <martins.ungurs@cube.lv>
2017-12-06 11:49:34 +0200

    [config]: add password as string identifier

--- src/Config/admin-log.php
+++ src/Config/admin-log.php
@@ -4,11 +4,11 @@ return [
     'sanitizer' => [
         'sensitive_string_identifiers' => [
             'XSRF-TOKEN',
-            config('session.cookie')
+            config('session.cookie'),
+            'password'
         ],
         'sensitive_key_patterns' => [
             '(\S*)password(\S*).*',
-            '(\S*)password(\S*)=*'
         ],
         'removed_value_notification' => 'value_removed_by_admin_log'
     ]
